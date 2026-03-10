423305728dc980ab5cb81254ba3362fe71b8c2bb
Nitheesh K <nitishkoda@outlook.com>
2026-03-09 23:31:35 -0500

    fix

--- src/config/frontend.tsx
+++ src/config/frontend.tsx
@@ -3,25 +3,29 @@ import ThirdParty, { Google } from "supertokens-auth-react/recipe/thirdparty";
 import Session from "supertokens-auth-react/recipe/session";
 import { SuperTokensConfig } from "supertokens-auth-react/lib/build/types";
 
+const apiDomain =
+  process.env.NEXT_PUBLIC_API_BASE_URL?.trim() || "http://localhost:8000";
+
+const websiteDomain =
+  process.env.NEXT_PUBLIC_BASE_URL?.trim() || "http://localhost:3000";
+
 export const frontendConfig: () => SuperTokensConfig = () => {
-    return {
-        appInfo: {
-            appName: "AggieSB+",
-            apiDomain: "http://localhost:8000",
-            websiteDomain: "http://localhost:3000",
-            apiBasePath: "/auth",
-            websiteBasePath: "/auth",
+  return {
+    appInfo: {
+      appName: "AggieSB+",
+      apiDomain,
+      websiteDomain,
+      apiBasePath: "/auth",
+      websiteBasePath: "/auth",
+    },
+    recipeList: [
+      EmailPassword.init(),
+      ThirdParty.init({
+        signInAndUpFeature: {
+          providers: [Google.init()],
         },
-        recipeList: [
-            EmailPassword.init(),
-            ThirdParty.init({
-                signInAndUpFeature: {
-                    providers: [
-                        Google.init(),
-                    ]
-                }
-            }),
-            Session.init(),
-        ],
-    };
+      }),
+      Session.init(),
+    ],
+  };
 };
